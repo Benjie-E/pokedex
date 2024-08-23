4fb527be84a91d1a1ad3e87050db1d8c7c435c42
Benjie Eisen <90060865+Benjie-E@users.noreply.github.com>
2024-08-23 05:40:51 -0400

    fixed deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // Switch to HashRouter
 import "./App.css";
 import Layout from "./layouts";
 import PokemonScreen from "./screens/PokemonScreen";
 import ErrorPage from "./error-page";
+
 const HomeScreen = lazy(() => import("./screens/HomeScreen"));
 
 const queryClient = new QueryClient();
@@ -12,23 +13,22 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <Router> {/* Make sure this is HashRouter */}
         <Routes>
-          <Route path={"/"} element={<Layout />}>
+          <Route path="/" element={<Layout />}>
             <Route
               index
-              path={"/"}
               element={
                 <Suspense fallback={<h1 className="text-5xl">Loading...</h1>}>
                   <HomeScreen />
                 </Suspense>
               }
             />
-            <Route path={"/pokemon/:id"} element={<PokemonScreen />} />
+            <Route path="pokemon/:id" element={<PokemonScreen />} />
           </Route>
-          <Route path="/*" element={<ErrorPage />} />
+          <Route path="*" element={<ErrorPage />} /> {/* Handle unknown routes */}
         </Routes>
-      </BrowserRouter>
+      </Router>
     </QueryClientProvider>
   );
 }
